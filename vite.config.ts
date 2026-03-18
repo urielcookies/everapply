f3f1fe8a1cf57122167befcdc4d04ce9b1011dd3
Ever Uriel Garcia <urielcookies@outlook.com>
2026-03-17 18:29:45 -0600

    fix: set base path to / for custom domain asset resolution

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
+  base: '/',
   plugins: [
     devtools(),
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
